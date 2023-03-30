e801931a3afab531318a107c5cdf311e290d7419
Yubo Du <duyubo@ad.unc.edu>
2023-03-29 21:17:23 -0400

    Progress on Exercise 07

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,49 @@
+"""EX07 - Dictionary Functions by Yubo Du my function skeletons and implementations below."""
+
+__author__ = "730621860"
+
+
+def invert(input1: dict[str,str]) -> dict[str,str]:
+    """a function used to swtich the keys and values of the dict we input."""
+    output_dict: dict[str,str] = dict()
+    for keys in input1:
+        output_dict[input1[keys]] = keys
+    if len(output_dict) != len(input1):
+        raise KeyError
+    return output_dict
+
+
+def favorite_color(input1: dict[str, str]) -> str:
+    """A function used to check what is the color that shows mostly in dictionary value."""
+    list_of_values: list[str] = []
+    
+    empty_dict: dict[str,int] = dict()
+   
+    for keys in input1:
+        list_of_values.append(input1[keys])
+    
+    for elements in list_of_values:
+        if elements in empty_dict:
+            empty_dict[elements] += 1
+        if elements not in empty_dict:
+            empty_dict[elements] = 1
+    most_frequncy_color: str = list_of_values[0]
+    for keys in empty_dict:
+        for key in empty_dict:
+            if empty_dict[keys] > empty_dict[key]:
+                most_frequncy_color = keys
+    return most_frequncy_color
+
+
+
+def count(input1: list[str]) -> dict[str, int]:
+    """A function used to check how many time a element shows in list and make it into a dictionary."""
+    empty_dict: dict[str,int] = dict()
+    for elements in input1:
+        if elements in empty_dict:
+            empty_dict[elements] += 1
+        if elements not in empty_dict:
+            empty_dict[elements] = 1
+    return empty_dict
+            
+            
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,63 @@
+"""EX07 - Dictionary Functions by Yubo Du Test file."""
+
+__author__ = "730621860"
+
+
+from exercises.ex07.dictionary import invert
+import pytest
+from exercises.ex07.dictionary import favorite_color
+from exercises.ex07.dictionary import count
+# The test below is used to test fuction invert.
+
+def test_normal_dict() -> None:
+    """Used to test the normal dictionary we input."""
+    assert invert({'a': 'm', 'b' : 'g', 'c': 'x'}) == {'m': 'a', 'g': 'b', 'x': 'c'}
+
+
+def test_KeyError() -> None:
+    """Used to test if it will output keyerror if the keys in output_dict may repeat."""
+    with pytest.raises(KeyError):
+        my_dictionary = {'kris': 'alex', 'michael': 'alex'}
+        invert(my_dictionary)
+
+
+def test_empty_dict() -> None:
+    """Used to test what will return if the input is empty dict."""
+    assert invert({}) == {}
+
+
+
+
+
+# The test below is used to test fuction favorite_colo.
+
+def test_normal_dict_check_2_color() -> None:
+    """Used to check when we input the normal dictionary."""
+    assert favorite_color({"Marc": "red", "Ezri": "red", "Kris": "blue"}) == "red"
+
+
+def test_when_exists_several_favorite_color() -> None:
+    """Used tp test if some colors shows same times."""
+    assert favorite_color({"Marc": "green", "Ezri": "red"}) == "green"
+
+
+def test_normal_dict_check_4_color() -> None:
+    """Used to test when 4 people gave the color they like."""
+    assert favorite_color({"Marc": "green", "Ezri": "red", "Bob": "green", "Alex": "yellow", "Alyx": "blue"}) == "green"
+
+
+# The test below is used to test fuction count.
+
+def test_normal_list_elements_shows_1_time() -> None:
+    """Used to test when we only input list with variables show single time."""
+    assert count(["pig", "cat", "cow"]) == {'pig': 1, 'cat': 1, 'cow': 1}
+
+
+def test_normal_list_elements_shows_several_time() -> None:
+    """Used to test when we input a list with variables show many times."""
+    assert count(["pig", "cat", "cow", "pig", "cat", "pig", "pig"]) == {'pig': 4, 'cat': 2, 'cow': 1}
+
+
+def test_empty_list() -> None:
+    """Used to test when we input empty list."""
+    assert count([]) == {}
